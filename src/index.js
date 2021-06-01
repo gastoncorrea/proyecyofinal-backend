2dd8e094ef53febb59354c4373148d99b0b9e0f0
Gaston <gastoncorrea90e@gmail.com>
2021-06-01 16:12:37 -0300

    Se hizo configuracion inicial de backend

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+app.set('port', process.env.PORT || 4001);
+app.listen(app.get('port'),()=>{
+    console.log('estoy en el puerto ' + app.get('port'));
+});
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'../public')));
+
+// rutas
+app.get('/',(req,res)=>{
+    res.send('hola esta es una prueba del backend');
+})
